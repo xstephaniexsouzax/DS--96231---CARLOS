5b85e418ded26373c512a8a38a74e2f9c978f834
stephanie <110237395+xstephaniexsouzax@users.noreply.github.com>
2026-04-29 21:13:39 -0300

    Aula do dia 28-04-2026

---
+++ DS-96231-X-DTO-CRIPTO/DS-96231-X-DTO-CRIPTO/projeto/src/main/java/com/example/projeto/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.projeto.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    //Permite o usu da criptografica na API
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    //Define quem pode acessar a API
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        return http.build();
+    }
+
+
+}
---
+++ DS-96231-X-DTO-CRIPTO/DS-96231-X-DTO-CRIPTO/projeto/src/main/java/com/example/projeto/service/UsuarioService.java
@@ -0,0 +1,39 @@
+package com.example.projeto.service;
+
+import com.example.projeto.dto.UsuarioRequestDTO;
+import com.example.projeto.dto.UsuarioResponseDTO;
+import com.example.projeto.model.UsuarioModel;
+import com.example.projeto.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsuarioService {
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private UsuarioRepository repository;
+
+    public List<UsuarioResponseDTO> listarTodos() {
+        return repository
+                .findAll()
+                .stream()
+                .map(u -> new UsuarioResponseDTO(u.getNome(), u.getEmail()))
+                .toList();
+    }
+
+    public UsuarioModel salvarUsuario(UsuarioRequestDTO usuarioDTO) {
+        if (repository.findByEmail(usuarioDTO.getEmail()).isPresent()) {
+            throw new RuntimeException("Usuário já cadastrado.");
+        }
+
+        UsuarioModel novoUsuario = new UsuarioModel();
+        novoUsuario.setNome(usuarioDTO.getNome());
+        novoUsuario.setEmail(usuarioDTO.getEmail());
+        novoUsuario.setSenha(passwordEncoder.encode(usuarioDTO.getSenha()));
+        return repository.save(novoUsuario);
+    }
+}
